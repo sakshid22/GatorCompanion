cd608d1ab61414200ed53b3aba6833390e941155
Sakshi Dubey <dubeysakshi22@gmail.com>
2018-01-06 22:14:01 -0500

    Phase 3
    Initial Phase of website

---
+++ about.php
@@ -0,0 +1,135 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+
+</head>
+
+<body>
+<body background="bg.png"
+<p>
+
+
+<a href="index.php">
+<IMG STYLE="position:absolute;
+			TOP:125px;
+			LEFT:210px; 
+			WIDTH:180px; 
+			HEIGHT:40px" 
+			SRC="home.jpg">
+
+</a>
+</p>
+
+<p>
+
+
+<a href="about.php">
+<IMG STYLE="position:absolute;
+			TOP:125px;
+			LEFT:390px; 
+			WIDTH:180px; 
+			HEIGHT:40px" 
+			SRC="about.jpg">
+
+</a>
+</p>
+
+<p>
+
+
+<a href="places.php">
+<IMG STYLE="position:absolute;
+			TOP:125px;
+			LEFT:570px; 
+			WIDTH:180px; 
+			HEIGHT:40px" 
+			SRC="places.jpg">
+
+</a>
+</p>
+
+<p>
+
+
+<a href="feedback.php">
+<IMG STYLE="position:absolute;
+			TOP:125px;
+			LEFT:750px; 
+			WIDTH:180px; 
+			HEIGHT:40px" 
+			SRC="feedback1.jpg">
+
+</a>
+</p>
+
+<p>
+
+
+<a href="developer.php">
+<IMG STYLE="position:absolute;
+			TOP:125px;
+			LEFT:930px; 
+			WIDTH:180px; 
+			HEIGHT:40px" 
+			SRC="developer.jpg">
+
+</a>
+</p>
+
+<a>
+<IMG STYLE="position:absolute;
+			TOP:165px;
+			LEFT:250px; 
+			WIDTH:750px; 
+			HEIGHT:80px" 
+			SRC="text.png">
+
+</a>
+</p>
+
+<a href="about_login.php">
+<IMG STYLE="position:absolute;
+			TOP:275px;
+		
+LEFT:290px; 
+			WIDTH:150px; 
+			HEIGHT:50px" 
+		src="newlogin.png">
+
+</a>
+</p>
+<a href="about_terms.php">
+<IMG STYLE="position:absolute;
+			TOP:325px;
+margin-top:10px;
+			LEFT:250px; 
+			WIDTH:215px; 
+			HEIGHT:50px" 
+
+			SRC="terms&c.png">
+
+</a>
+</p>
+<a href="feedback.php">
+<IMG STYLE="position:absolute;
+			TOP:375px;
+			LEFT:250px; 
+margin-top:20px;			
+WIDTH:215px; 
+			HEIGHT:50px" 
+
+			SRC="feedback.png">
+
+</a>
+</p>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
